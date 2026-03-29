14c00733ebdea2b7cb21b9c032afb887a65de5fb
Mrabeez <rabeezm56@gmail.com>
2026-03-29 22:54:17 +0530

    defined GPIO register and base addresses

---
+++ Drivers/STM32F401CCUx.h
@@ -0,0 +1,13 @@
+/*
+ * STM32F401CCUx.h
+ *
+ *  Created on: 28-Mar-2026
+ *      Author: Muhammad Rabeez
+ */
+
+#ifndef STM32F401CCUX_H_
+#define STM32F401CCUX_H_
+
+
+
+#endif /* STM32F401CCUX_H_ */
